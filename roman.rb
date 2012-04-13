34195693735ecaa1cf2c7244b323bc4b839b9562
Will <will@willubuntu.(none)>
2012-04-13 17:59:10 +0100

    refactored

--- roman.rb
+++ roman.rb
@@ -1,17 +1,19 @@
 class Roman
   def parse (number)
-    if number == 2
-      return 'II'
-    elsif number == 3
-      return 'III'
+    number_as_roman = ''
+    if number < 4
+      number_as_roman = parse_i_number (number)
     elsif number == 4
-      return 'IV'
+      number_as_roman ='IV'
     end
-    'I'    
+      number_as_roman
   end
 
   def parse_i_number (number)
-    @number = ''
-    number.do{ @number += 'I' }
+    number_as_roman = ''
+    for i in (1..number)
+      number_as_roman += 'I'
+    end
+    number_as_roman
   end
 end
